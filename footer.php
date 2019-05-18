82d9ba560bc36894ac95ba03ef174dc1017c99c7
altin <altin.t@gmail.com>
2019-05-18 20:00:03 +0200

    adds rss link to footer

--- footer.php
+++ footer.php
@@ -22,6 +22,7 @@
 			<?php if ( ! empty( $blog_info ) ) : ?>
 				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">&copy; <?php bloginfo( 'name' ); ?></a>
 			<?php endif; ?>
+				- <a class="site-name" href="<?php echo esc_url( home_url( '/feed' ) ); ?>" rel="home">RSS</a>
 			<?php
 			if ( function_exists( 'the_privacy_policy_link' ) ) {
 				the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
